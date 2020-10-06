54d8b5d6fce767a4a62a6a1e703626e4d52d7cd6
Dmitry Kalko <master-denn@mail.ru>
2020-10-06 21:14:12 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import SulaApp from './components/SulaApp';
+//import SulaApp from './components/SulaApp';
 import AdminPage from './components/AdminPage';
 
 ReactDOM.render(
 <BrowserRouter basename="/sula">
-          <Route path="/" exact component={SulaApp} />
+          {/* <Route path="/" exact component={SulaApp} /> */}
           <Route path="/admin" exact component={AdminPage} />
     </BrowserRouter>,
   document.getElementById('root')
