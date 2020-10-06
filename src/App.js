daedbb4d5d65a140e9b15ea5252f6aff6111e962
Dmitry Kalko <master-denn@mail.ru>
2020-10-06 22:03:16 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 import SulaApp from './components/SulaApp';
 import AdminPage from './components/AdminPage';
@@ -9,10 +9,10 @@ const App = () => {
  
   
     return (
-    <BrowserRouter basename="/sula">
+    <HashRouter basename="/sula">
           <Route path="/" exact component={SulaApp} />
           <Route path="/admin" exact component={AdminPage} />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
