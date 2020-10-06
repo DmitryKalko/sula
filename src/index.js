dc9689f69a263c52d7564246950078de123bb65e
Dmitry Kalko <master-denn@mail.ru>
2020-10-06 21:24:28 +0300

    delete build, fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-//import SulaApp from './components/SulaApp';
-//import AdminPage from './components/AdminPage';
 
 ReactDOM.render(
 <BrowserRouter basename="/sula">
