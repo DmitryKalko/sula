06fba15545012a6132ad1abaf407dfab96da28c0
Dmitry Kalko <master-denn@mail.ru>
2020-10-06 21:02:20 +0300

    use routing without App.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import SulaApp from './components/SulaApp';
+import AdminPage from './components/AdminPage';
+
 ReactDOM.render(
-  <BrowserRouter>
-  <App />
-</BrowserRouter>,
+<BrowserRouter basename="/sula">
+          <Route path="/" exact component={SulaApp} />
+          <Route path="/admin" exact component={AdminPage} />
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
