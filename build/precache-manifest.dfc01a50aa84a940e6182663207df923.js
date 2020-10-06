6020f0e5d97d635707aabf9d3cbc6b04d5906d45
Dmitry Kalko <master-denn@mail.ru>
2020-10-06 20:56:56 +0300

    deleted package2.json

---
+++ build/precache-manifest.dfc01a50aa84a940e6182663207df923.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4778cef482c265bee7017c5b05b929",
+    "url": "/sula/index.html"
+  },
+  {
+    "revision": "2068acb10616b758ab08",
+    "url": "/sula/static/css/main.22b8de4a.chunk.css"
+  },
+  {
+    "revision": "cdef97031084f873c043",
+    "url": "/sula/static/js/2.71df0e01.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/sula/static/js/2.71df0e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2068acb10616b758ab08",
+    "url": "/sula/static/js/main.e074c771.chunk.js"
+  },
+  {
+    "revision": "66aa047c05da758fa16e",
+    "url": "/sula/static/js/runtime-main.3a1b7025.js"
+  },
+  {
+    "revision": "f4b99cca2860c4c2e6ccfec1bdc6ee18",
+    "url": "/sula/static/media/logo.f4b99cca.png"
+  },
+  {
+    "revision": "8daccb5d93f2f35659bdddb8793bc9cb",
+    "url": "/sula/static/media/logoFooter.8daccb5d.png"
+  },
+  {
+    "revision": "a940ab03c12dd0246b98057b7d78e36e",
+    "url": "/sula/static/media/sertificate.a940ab03.png"
+  }
+]);
